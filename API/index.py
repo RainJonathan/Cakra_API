eb7aff40e2c69641d3598de2268da1bdde9b14d2
RainJonathan <reinjonathannn@gmail.com>
2024-05-03 19:17:40 +0700

    Reifne

--- API/index.py
+++ API/index.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify
-from controller.summerizer import generate_summary
+from flask_cors import CORS  # Import CORS class
 
+from controller.summerizer import generate_summary
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def home():
-    return 'Hello, Worlds!'
+    return 'Hello, World!'  # Corrected typo in return statement
 
 @app.route('/about')
 def about():
     return 'About'
 
-@app.route('/post_example', methods=['POST'])
+@app.route('/summarize', methods=['POST'])  # Corrected typo in route URL
 def post_example():
     # Access request data
     data = request.json  # assuming JSON data is sent
@@ -20,7 +22,7 @@ def post_example():
     # Process the data
     processed_data = generate_summary(payload_content)
     # Return a response
-    return jsonify({'error':False, 'message': 'Data Processed Succesfully', 'processed_data': processed_data})
+    return jsonify({'error': False, 'message': 'Data Processed Successfully', 'processed_data': processed_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
