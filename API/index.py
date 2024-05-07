71751a6ada54ca405c1dd189ef5a3ca8d705562d
RainJonathan <221110904@students.mikroskil.ac.id>
2024-05-07 16:37:06 +0700

    Added multi language support

--- API/index.py
+++ API/index.py
@@ -2,6 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS class
 
 from controller.summerizer import generate_summary
+
+from controller.language import EasyGoogleTranslate
+from langdetect import detect
+import re
+
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
@@ -13,16 +19,46 @@ def home():
 def about():
     return 'About'
 
+@app.route('/translate')
+def translate():
+    translator = EasyGoogleTranslate()
+    text = 'Hello, i am your student. Do you want to play football?'
+    result = translator.translate(text, target_language='id')
+    detected_language = (detect(result))
+    detected_language = re.sub(r'(?<=-)([a-z]+)', lambda x: x.group(0).upper(), detected_language)
+    print(detected_language)
+
+    print(translator.translate(text, target_language=detected_language))
+    return jsonify({'error': False, 'message': 'Data Processed Successfully', 'processed_data': translator.translate(text, target_language=detected_language)})
+
 @app.route('/summarize', methods=['POST'])  # Corrected typo in route URL
-def post_example():
-    # Access request data
+def summarize():
     data = request.json  # assuming JSON data is sent
     # Select the content of the "payload" field
-    payload_content = data.get('payload', '')  # If "payload" key doesn't exist, default to empty string
+    payloads = data.get('payload', [])  # If "payload" key doesn't exist, default to empty list
+    processed_data = []
+
     # Process the data
-    processed_data = generate_summary(payload_content)
+    for payload_content in payloads:
+        translator = EasyGoogleTranslate()
+        text = payload_content.get('text', '')
+
+        first_result = translator.translate(text, target_language='en') # Get the text from each payload object
+
+        summary = generate_summary(first_result)
+
+        detected_language = (detect(text))
+        detected_language = re.sub(r'(?<=-)([a-z]+)', lambda x: x.group(0).upper(), detected_language)
+        print(detected_language)
+
+        final_result = translator.translate(summary, target_language=detected_language)
+        processed_data.append(final_result)
+
     # Return a response
     return jsonify({'error': False, 'message': 'Data Processed Successfully', 'processed_data': processed_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
